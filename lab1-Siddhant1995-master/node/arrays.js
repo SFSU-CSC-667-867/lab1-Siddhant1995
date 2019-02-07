8f599df27031e388f757ebe84c678702a0085b36
Siddhant Padve <siddhantpadve@sfs-wifi-dhcp-10-143-225-227.wifi.sfsu.edu>
2019-02-06 20:37:52 -0800

    Commit

--- lab1-Siddhant1995-master/node/arrays.js
+++ lab1-Siddhant1995-master/node/arrays.js
@@ -1,29 +1,38 @@
 // Arrays in js
 
 // arrays can be delared explicitly
-const array = [9, 3, 4];
+const arrayTest = [9, 3, 4];
+arrayTest[3] = 9;
 
 // or with new keyword
 const anotherArray = new Array(5);
 
 // access array elements
-console.log(array[0]);
+console.log(arrayTest[0]);
 
 // change elements
-array[1] = 'hello';
+arrayTest[1] = 'hello';
 
 // for practice
 
 // use forEach
-
+arrayTest.forEach((n)=>{
+    console.log(n);
+});
 // use map
+const newArray = arrayTest.map(i => i + 1);
+console.log(newArray);
 
 // use pop
-
+newArray.pop();
+console.log(newArray);
 // use push
-
+newArray.push(4);
+console.log(newArray);
 // use shift
-
+newArray.shift();
+console.log(newArray);
 // use unshift
-
+newArray.unshift(10);
+console.log(newArray);
 // use filter
